ab69ae288833335119554f90750c61fd5391a5df
grace866 <grtttan@gmail.com>
2026-09-06 08:55:39 -0400

    Add performance analysis graphs

--- src/main.cpp
+++ src/main.cpp
@@ -23,8 +23,8 @@
 
 // LOOK-2.1 LOOK-2.3 - toggles for UNIFORM_GRID and COHERENT_GRID
 #define VISUALIZE 1
-#define UNIFORM_GRID 1
-#define COHERENT_GRID 1
+#define UNIFORM_GRID 0
+#define COHERENT_GRID 0
 
 // LOOK-1.2 - change this to adjust particle count in the simulation
 const int N_FOR_VIS = 100000;
@@ -246,11 +246,11 @@ void initShaders(GLuint * program) {
         frame = 0;
       }
 
-      /*if (time - timestart > 10.0) {
+      if (time - timestart > 10.0) {
           double avg_fps = totalframes / (time - timestart);
           printf("average fps %f\n", avg_fps);
           glfwSetWindowShouldClose(window, GLFW_TRUE);
-      }*/
+      }
 
       runCUDA();
 
